5553ea01cda6626d8d38d2d0863dd5e41cac6d84
aaronmdenny <aaronmdenny@outlook.com>
2021-07-06 20:51:11 -0400

    how many instances of service should it be?

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -3,11 +3,16 @@ import { Component, Input } from '@angular/core';
 import { AccountsService } from '../accounts.service';
 import { LoggingService } from '../logging.service';
 
+/**
+ * Removing the AccountsService from the providers array in the child components of the AppComponent ensures that the
+ * child components will use the same instance of the service as in AppComponent, and now, adding or updating an account
+ * will work as expected in the template.
+ */
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers: [AccountsService, LoggingService]
+  providers: [LoggingService]
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
