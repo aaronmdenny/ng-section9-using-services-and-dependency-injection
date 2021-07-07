eb9c0b75a29e7a0a9b4c7acf7381936038bd14bf
aaronmdenny <aaronmdenny@outlook.com>
2021-07-06 20:46:51 -0400

    understanding the hierarchical injector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,18 @@ export class AppComponent implements OnInit {
      * When we add an account in the new-account component, the list of accounts does not update in the template.
      * 
      * Similarly, when we update the status of an account, the status does not update in the template.
+     * 
+     * This is because the dependency injection in Angular is hierarchical. If we provide a service on a component, the
+     * Angular Framework creates an instance of the service for that component and all of its children components.
+     * 
+     * The highest possible level of providing a service is the providers array of the AppModule, where providing a
+     * service will create an instance of the service for the entire application.
+     * 
+     * The next level down is the AppComponent. There, the AppComponent and all of its child components will use the
+     * same instance of the service. However, other services using this service will not get this instance.
+     * 
+     * The next level down is any other component, in which case, the same instance will be shared among this component
+     * and all of its children components.
      */
     this.accounts = this.accountsService.accounts;
   }
