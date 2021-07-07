deb013a6c0c2b61441490840baa540ecbc9ecc62
aaronmdenny <aaronmdenny@outlook.com>
2021-07-06 21:06:27 -0400

    injecting services into services

--- src/app/accounts.service.ts
+++ src/app/accounts.service.ts
@@ -1,6 +1,20 @@
+import { Injectable } from "@angular/core";
+
+import { LoggingService } from "./logging.service";
+
 /**
  * Services are also used to consolidate data that needs to be accessed by multiple components.
  */
+/* 
+ * When you inject a service into some class, you have to include some metadata in a decorator. Creating a service does
+ * not require you to have a metadata decorator like a component or a directive, so we have a decorator for this special
+ * circumstance. This is the @injectable() decorator.
+ * 
+ * You do not need to add @Injectable in the service you want to inject, but rather in the service where another service
+ * will be injected. In newer versions of Angular, however, it is recommended that you always add @injectable to a
+ * service.
+ */
+@Injectable()
 export class AccountsService {
     accounts = [
         {
@@ -17,14 +31,21 @@ export class AccountsService {
         }
     ];
 
+    /**
+     * Add the logging service here, injected from the AppModule.
+     */
+    constructor(private loggingService: LoggingService) { }
+
     addAccount(name: string, status: string) {
         this.accounts.push({
             name: name,
             status: status
         });
+        this.loggingService.logStatusChange(status);
     }
 
     updateStatus(id: number, status: string) {
         this.accounts[id].status = status;
+        this.loggingService.logStatusChange(status);
     }
 }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -10,7 +10,7 @@ import { LoggingService } from '../logging.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  //providers: [LoggingService]
 })
 export class NewAccountComponent {
   /**
@@ -23,6 +23,6 @@ export class NewAccountComponent {
    */
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
+    //this.loggingService.logStatusChange(accountStatus);
   }
 }
